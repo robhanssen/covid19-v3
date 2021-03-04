a93fb097c07b855f60d387b37d58755bdb727999
Rob Hanssen <rob.hanssen@outlook.com>
2021-03-04 13:27:35 -0500

    case extrapolation for SC

--- process_us_data.r
+++ process_us_data.r
@@ -175,9 +175,19 @@ casesdeathsbylocation %>% filter(date > today() - months(6)) %>%
                         geom_line(aes(date, correction*deathsper100k), color="red", linetype="dotted")  + 
                         geom_line(aes(y=rollmean(correction*deathsper100k,avdays,na.pad=TRUE)), size=2, color="red") 
 
+write_csv(casesdeathsbylocation, "data/sc-casesdeath.csv")
 ggsave("graphs/covid19-SC-cases-and-deaths.pdf")
         
-
+casesdeathsbylocation %>% filter(date > as.Date("2021-01-21")) %>% 
+                        ggplot + aes(x=date, y=casesper100k) + geom_point() + geom_smooth(method="lm", fullrange=TRUE) + 
+                        scale_y_continuous(limit=c(-50,100), breaks=seq(0,100,10)) + 
+                        scale_x_date(breaks="2 weeks", date_labels="%b %d", limit=as.Date(c("2021-01-21","2021-04-07"))) +
+                        labs(x="Date", y="Cases per 100k population", title="Cases in South Carolina", subtitle="Cases per 100,000") +
+                        geom_hline(yintercept=10, lty=2) +
+                        geom_hline(yintercept=5, lty=3) +
+                        geom_hline(yintercept=0, lty=1) +
+                        geom_vline(xintercept=as.Date("2021-03-09"),lty=2) + geom_vline(xintercept=as.Date("2021-04-02"),lty=2) 
+ggsave("graphs/covid19-SC-casesextrapolation.pdf")
 # 
 # 
 # data per for South Carolina (Spartanburg/Greenville)
