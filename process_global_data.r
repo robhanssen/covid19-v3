205d83f43975ea5d7f185397bb41b1ecea0c5a83
Rob Hanssen <rob.hanssen@outlook.com>
2021-04-26 09:40:45 -0400

    global graph with India/LatAm cutout

--- process_global_data.r
+++ process_global_data.r
@@ -23,23 +23,29 @@ avdays=7
 totalcasecomment=""
 capt="insert caption here"
 # world cases and deaths growth
-casesdeaths %>% group_by(date,time) %>%
+timecutoff = today() - months(12)
+
+casesdeaths %>% filter(date > timecutoff) %>%
+                mutate(partofworld = ifelse(country=="India" | continent=="Latin America", "India and Latin America", "Rest of World")) %>% 
+                filter(!is.na(partofworld)) %>%
+                group_by(date,time,partofworld) %>%
                 summarize(cases=sum(cases), 
                           deaths=sum(deaths),
                           population=sum(population, na.rm=T),
                           casesper100k = cases/population * 1e5,
                           deathsper100k = deaths/population * 1e5
                           ) %>% 
-                ggplot + aes(date, casesper100k) + geom_line(color="blue", linetype="dotted") + geom_line(aes(y=rollmean(casesper100k,avdays, na.pad=TRUE)), size=2, color="blue") + 
-                        scale_y_continuous(sec.axis = sec_axis(~ ./correction, breaks=seq(0,5,1))) + #scale_y_log10(limit=c(10,100000))+ 
+                ggplot + aes(date, cases, color=partofworld) + geom_line() + #geom_line(aes(y=rollmean(cases,avdays, na.pad=TRUE)), size=2, color="blue") + 
+                        scale_y_continuous(limit=c(0,1e6)) + #scale_y_log10(limit=c(10,100000))+ 
                         scale_x_date(date_breaks="3 months", date_labels = "%b %d") + #facet_wrap(~location) + 
-                        labs(caption=capt) + xlab("Date") + ylab("Daily incremental number of confirmed cases or deaths") +
-                        ggtitle(paste("Global daily cases and deaths with", avdays,"days average line")) + 
-                        geom_line(aes(date, correction*deathsper100k), color="red", linetype="dotted")  + geom_line(aes(y=rollmean(correction*deathsper100k,avdays,na.pad=TRUE)), size=2, color="red") +
+                        labs(caption=capt) + xlab("Date") + ylab("Daily incremental number of confirmed cases or deaths") + labs(color="Part of world") + 
+                        ggtitle(paste("Global daily cases and deaths with", avdays,"days average line")) #+ 
+                        #geom_line(aes(date, correction*deaths), color="red", linetype="dotted")  + geom_line(aes(y=rollmean(correction*deaths,avdays,na.pad=TRUE)), size=2, color="red") +
                         #annotate("text",x=as.Date("2020-03-15", format="%Y-%m-%d"),y=20000,label="cases\n<-----", color="blue") + 
                         #annotate("text",x=as.Date("2020-04-10", format="%Y-%m-%d"),y=10000,label="deaths\n------>", color="red") +
-                        annotate("text",x=as.Date("2020-03-28", format="%Y-%m-%d"),y=20,label=totalcasecomment, color="black")
+                        #annotate("text",x=as.Date("2020-03-28", format="%Y-%m-%d"),y=20,label=totalcasecomment, color="black")
 
+ggsave("graphs/covid19-global-infections.pdf")
 
 correction = 50
 
