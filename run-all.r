d06ef32d1eb51607c230101fb4ecdfd1860dabc2
Rob Hanssen <rob.hanssen@outlook.com>
2021-05-13 13:05:24 -0400

    new data gen after extrapolation update

--- run-all.r
+++ run-all.r
@@ -5,16 +5,16 @@ if(lastreadus != today())
 {
         print("READING NEW DATA FOR US")
         source("import-us-data.r")
+
 }
 source("process_us_data.r")
-
+source("extrapolation.r")        
 
 load("Rdata/global_casesdeaths.Rdata")
 if (lastreadglobal != today())
 {
         print("READING NEW DATA FOR GLOBAL")
         source("import-global-data.r")
-        source("extrapolation.r")
 }
 source("process_global_data.r")
 
