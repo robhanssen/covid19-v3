4e36a37ac1f3c81f32460c62575e86f8a332a489
Rob Hanssen <rob.hanssen@outlook.com>
2021-06-04 14:17:47 -0400

    added "0" expectation date

--- extrapolation.r
+++ extrapolation.r
@@ -1,6 +1,6 @@
 library(broom)
 
-find_value <- function(x,y,target=c(2,5,10)) {
+find_value <- function(x,y,target=c(0, 2,5,10)) {
     aa <- approx(y,x,xout=target)$y
     as.Date(aa,origin="1970-01-01")  ## convert back to a date (ugh)
 }
