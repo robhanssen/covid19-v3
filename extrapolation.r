649abda793eea28dab455e50295968a90856a18d
Rob Hanssen <rob.hanssen@outlook.com>
2021-07-11 17:11:05 -0400

    new extrapolation target (up)

--- extrapolation.r
+++ extrapolation.r
@@ -9,7 +9,7 @@ find_value <- function(x,y,target=c(0, 2,5,10)) {
 #  extrapolocation for SC data
 # 
 
-us_casesdeaths %>% filter(state=="South Carolina", date>as.Date("2021-03-01")) %>%
+us_casesdeaths %>% filter(state=="South Carolina", date>as.Date("2021-06-15")) %>%
                         group_by(date) %>%
                         summarize(population = sum(population),
                                   cases = sum(cases),
@@ -35,18 +35,20 @@ us_casesdeaths %>% filter(state=="South Carolina", date>as.Date("2021-03-01")) %
 #     %>% pivot_longer(cols=everything(),names_to="limit",values_to="date")
 # )
 
-lims <- cdbl %>%
+linmod <- cdbl %>%
     ## fit linear model
     lm(formula=casesper100k~date) %>%
     ## predict/add confidence intervals
     augment(interval="confidence",
                 newdata=data.frame(date=
                  seq.Date(from=min(cdbl$date),to=max(cdbl$date)+120,
-                              by="1 day"))) %>%
+                              by="1 day"))) 
+                              
+lims <- linmod %>%
     select(date,.lower,.upper) %>%
     ## interpolate to find date corresponding to target value (10)
     ## should use across() but I can't get it working
-    summarise(lwr=find_value(date,.lower),
+    summarise(lwr=find_value(date,.upper),
                   upr=find_value(date,.upper)) %>%
     ## convert to useful data frame for ggplot
     pivot_longer(cols=everything(),names_to="limit",values_to="date") %>% filter(limit=="upr")
@@ -59,6 +61,7 @@ lims <- cdbl %>%
     + geom_point()
     + expand_limits(x=max(cdbl$date+60))
     + geom_smooth(method="lm", fullrange=TRUE)
+    # + geom_line(data=linmod, aes(y=.fitted), color="blue", lty=2)
     + scale_y_continuous(limit=c(-10,40), breaks=seq(0,100,10))
     + scale_x_date(breaks="2 weeks", date_labels="%b %d")
     + geom_hline(yintercept=10,lty=2)
