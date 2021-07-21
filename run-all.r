31031d8eb79718f683321980bd4eda3f1d8fa30d
Rob Hanssen <rob.hanssen@outlook.com>
2021-07-21 08:33:37 -0400

    update 7/21

--- run-all.r
+++ run-all.r
@@ -1,22 +1,18 @@
 library(lubridate)
 
 load("Rdata/us_casesdeaths.Rdata")
-if(lastreadus != today())
-{
+if (lastreadus != today()) {
         print("READING NEW DATA FOR US")
         source("import-us-data.r")
+        }
 
-}
 source("process_us_data.r")
-source("extrapolation.r")        
+source("extrapolation.r")
 
 load("Rdata/global_casesdeaths.Rdata")
-if (lastreadglobal != today())
-{
+if (lastreadglobal != today()) {
         print("READING NEW DATA FOR GLOBAL")
         source("import-global-data.r")
-}
+        }
+
 source("process_global_data.r")
-
-
-
