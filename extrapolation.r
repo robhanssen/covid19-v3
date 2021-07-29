0d64cb68ab61fc15a86b3b4161ed022167c4d6ae
Rob Hanssen <rob.hanssen@outlook.com>
2021-07-29 08:54:33 -0400

    new extrapolation; data update 7/29

--- extrapolation.r
+++ extrapolation.r
@@ -232,3 +232,42 @@ ggplot(data = cdbl_gsp) +
     geom_hline(yintercept = 10, color = "black", lty = 2)
 
 ggsave("projections/covid19-SCGSP-linearpredict14days.pdf", width=11, height=8)
+
+
+#
+# 14 day trends in SC
+#
+
+# twoweeksago <- today() - weeks(2)
+# twoweeksfromnow <- today() + weeks(2)
+
+# next2weeks <- tibble(date = seq.Date(from = twoweeksago,
+#                                     to = twoweeksfromnow,
+#                                     by = "1 day"))
+
+predict_sc <- cdbl %>%
+        filter(date >= twoweeksago) %>%
+        lm(casesper100k ~ date, data=.) %>%
+        augment(newdata = next2weeks, interval = "confidence")
+
+date10 = find_value(predict_sc$date, predict_sc$.fitted, target=10)
+
+ggplot(data = cdbl) +
+    aes(x = date, y = casesper100k) +
+    geom_point() +
+    geom_line(data = cdbl, aes(y = zoo::rollmean(casesper100k, 14, na.pad = TRUE, align = "right")), color = "red") +
+    geom_line(data = cdbl, aes(y = zoo::rollmean(casesper100k, 14, na.pad = TRUE, align = "center")), color = "red", lty = 2) +        
+    expand_limits(x = max(cdbl$date + 20)) +
+    scale_y_continuous(limit = c(0, 40), breaks = seq(0, 100, 10)) +
+    scale_x_date(breaks = "2 weeks", date_labels = "%b %d") +
+    geom_line(data = predict_gsp, aes(y = .fitted), color = "darkgreen") + 
+    geom_ribbon(data = predict_gsp, aes(y = .fitted, ymin = .lower, ymax = .upper), fill = "green", alpha = 0.4) +
+    labs(x = "Date",
+        y = "Cases per 100k population",
+        title = "Cases in South Carolina",
+        subtitle = "Cases per 100,000",
+        caption = "Red line: rolling mean (14 days)\nGreen line: prediction based on 14 previous days") + 
+    geom_vline(xintercept =  date10, color = "red", lty = 2) + 
+    geom_hline(yintercept = 10, color = "black", lty = 2)
+
+ggsave("projections/covid19-SC-linearpredict14days.pdf", width=11, height=8)
