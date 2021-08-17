1106ebf26355aced8e5104f7ba8612a096edb054
Rob Hanssen <rob.hanssen@outlook.com>
2021-08-17 08:50:59 -0400

    update 8/17

--- misc/comparison_of_countries_and_states.r
+++ misc/comparison_of_countries_and_states.r
@@ -23,3 +23,14 @@ combined_countryanstates %>% top_n(30,casesper100k) %>%
                                scale_fill_manual(values=colorset)
 
 ggsave("misc/comparison-of-countries-and-states.pdf", height = 11, width = 8)
+
+combined_countryanstates %>% top_n(30,deathsper100k) %>%
+                        arrange(-deathsper100k) %>% bind_cols(rank=1:30) %>% mutate(country=paste0(country," (",rank,")")) %>%
+                        ggplot + aes(x=fct_reorder(country,deathsper100k), y=deathsper100k) + 
+                               scale_y_continuous(breaks=seq(0,2,.2)) +
+                               geom_bar(stat="identity") + 
+                               labs(x="Countries with population over 5 million and US states", y="Daily new deaths per 100,000 population", caption=caption) +
+                               coord_flip() 
+
+
+ggsave("misc/comparison-of-countries-and-states-by-deaths.pdf", height = 11, width = 8)
