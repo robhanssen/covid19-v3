e0870feb6a010b97d5c0b9234b99c264ad5d0fed
Rob Hanssen <rob.hanssen@outlook.com>
2021-08-24 15:36:12 -0400

    more fun with maps

--- misc/us_map.r
+++ misc/us_map.r
@@ -5,12 +5,10 @@ library(broom)
 
 load("Rdata/us_casesdeaths.Rdata")
 
-
 counties <- us_casesdeaths %>%
         select(state, county, population) %>%
         unique() %>%
         mutate(state = toupper(state), county = toupper(county))
-
 
 countymapdata <- as_tibble(map_data("county")) %>% 
                         rename(state = region, county = subregion) %>%
@@ -68,3 +66,31 @@ ggplot(data = orderedcountymapdata) +
   theme(legend.position = "none")
   
   ggsave("misc/map_us_80.png")
+
+
+
+  ########################
+
+countyarea <- read_csv("misc/countyarea.csv") %>%
+                          mutate(state = toupper(state), county = toupper(county))
+
+totalarea <- sum(countyarea$area)
+
+
+
+orderedcounties <- counties %>% inner_join(countyarea) %>%
+        arrange(-population) %>%
+        mutate(cumpop = cumsum(population), cumarea = cumsum(area)) %>%
+        mutate(pop_frac = cumpop / totalpop, area_frac = cumarea / totalarea)
+
+
+orderedcounties %>%
+        ggplot +
+          aes(y = pop_frac, x = area_frac) +
+          geom_line() + 
+          theme_light() +
+          labs(y = "Fraction of population", x = "Fraction of land area") + 
+          scale_x_continuous(labels = scales::percent, breaks = .2 * 0:5) + 
+          scale_y_continuous(labels = scales::percent, breaks = .2 * 0:5) +
+          geom_vline(xintercept = .2, lty = 2) +
+          geom_hline(yintercept = .8, lty = 2)
