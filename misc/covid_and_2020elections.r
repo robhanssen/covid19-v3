9b32a33ba72938719c53900b248e5e6adafdf6e0
Rob Hanssen <rob.hanssen@outlook.com>
2021-09-16 16:19:52 -0400

    added pandemic stage info

--- misc/covid_and_2020elections.r
+++ misc/covid_and_2020elections.r
@@ -100,3 +100,69 @@ casespercounty %>%
         ggplot + 
         aes(x = per_gop, y = casesper100k) + geom_point() + 
         geom_smooth(method = "lm")
+
+####################
+
+cuttoffdate <- as.Date("2020-07-01")
+
+electionresults <- read_csv("sources/2020electionresults.csv") %>% 
+        mutate(trumpvictory = cut(per_gop, c(0,0.1 * 1:10), 
+                                           c("<10%", "10-20","20-30","30-40","40-50","50-60","60-70","70-80","80-90",">90%"))) %>%
+        mutate(trumpvictory = factor(trumpvictory)) %>%
+        select(state, county, trumpvictory, per_gop)
+
+# statepop <- us_casesdeaths %>%
+#                 select(state, county, population) %>%
+#                 unique() %>% 
+#                 group_by(state, county) %>%
+#                 summarize(population = sum(population))
+
+statepop <- us_casesdeaths %>%
+                pivot_wider(c(county, state,population))
+
+
+# define constants
+
+casesperelection <- us_casesdeaths %>% mutate(stage = ifelse(date < cuttoffdate, "early", "late"), stage = factor(stage, levels = c("early", "late"))) %>%
+        inner_join(electionresults) %>%
+        group_by(county, state, trumpvictory, stage) %>%
+        summarize(cases = sum(cases), 
+                  deaths = sum(deaths)) %>%
+        inner_join((statepop)) %>%
+        ungroup() %>%
+        group_by(trumpvictory, stage) %>%
+        summarize(cases = sum(cases), deaths = sum(deaths), population = sum(population)) %>%
+                mutate(
+                  casesper100k = cases / population * 1e5,
+                  deathsper100k = deaths / population * 1e5
+                  )
+
+stagecolor = c("early" = "darkgreen", "late" = "purple")
+colors = c("<25%" = "blue", "25-50%" = "#F09620", "50-75%" = "#e300f8", ">75%" = "red")
+
+casesperelection %>% 
+    ggplot +
+        aes(trumpvictory, deathsper100k, fill = stage) +
+        geom_col() +
+        labs(title = "What happens when virus response is politicized?",
+             fill = "Pandemic stage",   
+             x = "Percentage of votes for Trump in 2020 elections",
+             y = "Cumulative COVID-19 deaths per 100,000\nafter Election Day 2020",
+             caption = paste0("COVID-19 deaths from Election Day 2020 until ", format(today(), format = "%b %d, %Y"))) +
+        theme_light() +
+        scale_fill_manual(values = stagecolor) #+
+        # theme(legend.position = "none")
+
+#ggsave("misc/deathsbyelectionresults.png", width = 6, height = 6)
+
+casesperelection %>%
+    ggplot +
+        aes(trumpvictory, casesper100k, fill = stage) +
+        geom_col() +
+        labs(title = "What happens when virus response is politicized?",
+             x = "Percentage of votes for Trump in 2020 elections",
+             y = "Cumulative COVID-19 cases per 100,000\nafter Election Day 2020",
+             caption = paste0("COVID-19 cases from Election Day 2020 until ", format(today(), format = "%b %d, %Y"))) +
+        theme_light() +
+        scale_fill_manual(values = stagecolor) +
+        theme(legend.position = "none")
