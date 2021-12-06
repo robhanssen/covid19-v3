735872711833a3296325d7d08d4495a6e1aa0b6b
Rob Hanssen <rob.hanssen@outlook.com>
2021-12-06 10:47:20 -0500

    info by state

--- misc/infections_by_state.r
+++ misc/infections_by_state.r
@@ -32,3 +32,27 @@ us_casesdeaths %>%
     theme(legend.position = "none") 
 
 ggsave("misc/cumulative-infections-by-state.png", width = 6, height = 10)
+
+
+totalcases = sum(us_casesdeaths$deaths)
+avcases = totalcases / countrypop
+
+
+us_casesdeaths %>%
+    group_by(state) %>%
+    summarize(cases = sum(deaths), .groups = "drop") %>%
+    inner_join(statepop, by = "state") %>%
+    mutate(color = case_when(state == "South Carolina" ~ "red", TRUE ~ "gray50")) %>%
+    mutate(bystate = cases/population) %>%
+    ggplot + 
+    aes(fct_reorder(state, bystate), bystate, fill = color) + 
+    geom_col() + 
+    scale_y_continuous(labels = scales::percent_format(accuracy = .1)) +
+    labs(y = "Casualties", x = "state", caption = "JHU data, up until Dec 6, 2021. Vertical line is country average") + 
+    geom_hline(yintercept = avcases, lty = 2, color = "black") +
+    scale_fill_manual(values = c("red" = "red", "gray50" = "gray50")) +
+    coord_flip() +
+    theme_light() + 
+    theme(legend.position = "none") 
+
+ggsave("misc/cumulative-deaths-by-state.png", width = 6, height = 10)
