f65bfcffc7d3941b8357e468b72ca3fbb99df3cc
Rob Hanssen <rob.hanssen@outlook.com>
2022-01-04 10:55:33 -0500

    updated infections by US state

--- misc/infections_by_state.r
+++ misc/infections_by_state.r
@@ -1,7 +1,7 @@
 library(tidyverse)
 load("Rdata/us_casesdeaths.Rdata")
 
-us_casesdeaths
+us_casesdeaths 
 
 statepop <-
     us_casesdeaths %>%
@@ -14,6 +14,10 @@ countrypop = sum(statepop$population)
 totalcases = sum(us_casesdeaths$cases)
 avcases = totalcases / countrypop
 
+lastdate <- format(max(us_casesdeaths$date), format = "%b %d, %Y")
+caption_text <- paste0("JHU data, up until ", lastdate, ". Vertical line is country average")
+
+
 us_casesdeaths %>%
     group_by(state) %>%
     summarize(cases = sum(cases), .groups = "drop") %>%
@@ -24,7 +28,7 @@ us_casesdeaths %>%
     aes(fct_reorder(state, bystate), bystate, fill = color) + 
     geom_col() + 
     scale_y_continuous(labels = scales::percent_format(accuracy = .1)) +
-    labs(y = "Diagnosed cases", x = "state", caption = "JHU data, up until Dec 6, 2021. Vertical line is country average") + 
+    labs(y = "Diagnosed cases", x = "state", caption = caption_text) + 
     geom_hline(yintercept = avcases, lty = 2, color = "black") +
     scale_fill_manual(values = c("red" = "red", "gray50" = "gray50")) +
     coord_flip() +
@@ -43,12 +47,12 @@ us_casesdeaths %>%
     summarize(cases = sum(deaths), .groups = "drop") %>%
     inner_join(statepop, by = "state") %>%
     mutate(color = case_when(state == "South Carolina" ~ "red", TRUE ~ "gray50")) %>%
-    mutate(bystate = cases/population) %>%
+    mutate(bystate = cases / population) %>%
     ggplot + 
     aes(fct_reorder(state, bystate), bystate, fill = color) + 
     geom_col() + 
     scale_y_continuous(labels = scales::percent_format(accuracy = .1)) +
-    labs(y = "Casualties", x = "state", caption = "JHU data, up until Dec 6, 2021. Vertical line is country average") + 
+    labs(y = "Casualties", x = "state", caption = caption_text) + 
     geom_hline(yintercept = avcases, lty = 2, color = "black") +
     scale_fill_manual(values = c("red" = "red", "gray50" = "gray50")) +
     coord_flip() +
@@ -89,7 +93,7 @@ us_casesdeaths %>%
     aes(fct_reorder(countyname, bystate), bystate, fill = color) +
     geom_col() +
     scale_y_continuous(labels = scales::percent_format(accuracy = .1)) +
-    labs(y = "Diagnosed cases", x = "County", caption = "JHU data, up until Dec 6, 2021. Vertical line is country average") + 
+    labs(y = "Diagnosed cases", x = "County", caption = caption_text) + 
     geom_hline(yintercept = avcases, lty = 2, color = "black") +
     scale_fill_manual(values = c("red" = "red", "gray50" = "gray50")) +
     coord_flip() +
